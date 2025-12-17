45ea98fe91beb6dede60b5e3271539d35571d2e0
Sothearo HEM <hem.sothearo.1223@rupp.edu.kh>
2025-12-18 00:43:35 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.forEach(nav => nav.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
